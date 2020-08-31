270faa94948e341e666a2d81bbffb52aea33eb2c
Angelo <angelo.andreussi@gmail.com>
2020-08-31 18:42:38 +0200

    aggiunta prod

--- src/my_util.erl
+++ src/my_util.erl
@@ -1,8 +1,11 @@
 -module(my_util).
--export([println/1, add/2]).
+-export([println/1, add/2, prod/2]).
 
 println(What) -> 
 	io:format("Messaggio: ~p~n", [What]).
 
 add(A,B) ->
 	A + B.
+
+prod(A,B) ->
+	A * B.
