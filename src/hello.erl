2a6c6371fbd19bc8534535f0c06b9d36b928079c
Alessandro <aleforgia3@gmail.com>
2020-09-03 12:14:33 +0200

    Prove pattern matching

--- src/hello.erl
+++ src/hello.erl
@@ -2,7 +2,22 @@
 
 -module(hello).
 -import('my_util', [println/1]).
--export([hello_world/0]).
+-export([hello_world/0, hello2/0, hello3/0,  hello4/0,  hello5/0, hello6/0]).
 
 hello_world() ->
 	my_util:println("Hello, World!").
+
+hello2() ->
+	io:format("~s~n",[<<"Hello">>]).
+
+hello3() ->
+	io:format("~p~n",[<<"Hello">>]).
+
+hello4() ->
+	io:format("~~~n").
+
+hello5() ->
+	io:format("~f~n", [4.0]).
+
+hello6() ->
+	io:format("~30f~n", [4.0]).
--- src/my_util.erl
+++ src/my_util.erl
@@ -1,5 +1,5 @@
 -module(my_util).
--export([println/1, add/2, prod/2]).
+-export([println/1, add/2, prod/2, temperature_converter/2, temperature_converter/3]).
 
 println(What) -> 
 	io:format("Messaggio: ~p~n", [What]).
@@ -9,3 +9,19 @@ add(A,B) ->
 
 prod(A,B) ->
 	A * B.
+
+temperature_converter({Current_scale, T}, Target_scale) ->
+	temperature_converter(Current_scale, T, Target_scale).
+
+temperature_converter(kelvin, T, kelvin) -> T;
+temperature_converter(kelvin, T, celsius) ->  T - 273.15;
+temperature_converter(kelvin, T, fahrenheit) -> (T - 273.15) * 9 / 5 + 32;
+
+temperature_converter(celsius, T, kelvin) -> T + 273.15;
+temperature_converter(celsius, T, celsius) ->  T;
+temperature_converter(celsius, T, fahrenheit) -> T * 9 / 5 + 32;
+
+temperature_converter(fahrenheit, T, kelvin) -> (T + 459.67) * 5 / 9;
+temperature_converter(fahrenheit, T, celsius) ->  T - (T - 32) * 5 / 9;
+temperature_converter(fahrenheit, T, fahrenheit) -> T.
+
