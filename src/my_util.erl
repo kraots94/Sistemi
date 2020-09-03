9d34d1a7b04ec3344d2906fd59cf94a158b805bc
AgnulFisso <angelo.andreussi@gmail.com>
2020-09-03 13:13:22 +0200

    2 test significativi

--- src/my_util.erl
+++ src/my_util.erl
@@ -1,5 +1,6 @@
 -module(my_util).
--export([println/1, add/2, prod/2, temperature_converter/2, temperature_converter/3]).
+-export([println/1, add/2, prod/2, temperature_converter/2, temperature_converter/3
+		,test_patternMatchingConPluriMatch/1,test_caseRitornaValore/1]).
 
 println(What) -> 
 	io:format("Messaggio: ~p~n", [What]).
@@ -25,3 +26,18 @@ temperature_converter(fahrenheit, T, kelvin) -> (T + 459.67) * 5 / 9;
 temperature_converter(fahrenheit, T, celsius) ->  T - (T - 32) * 5 / 9;
 temperature_converter(fahrenheit, T, fahrenheit) -> T.
 
+%test di matching dove ci sono 2 match, infatti con input {preciso,T}
+%il match avviene con entrambe le funzioni ma viene selezionata la prima che matcha in ordine di scrittura 
+test_patternMatchingConPluriMatch({preciso, T}) -> println(T);
+%underscore inserito per non avere warning di mancato utilizzo var input
+test_patternMatchingConPluriMatch(_Tupla) -> println("non preciso").
+
+%dimostrazione che case (ma anche if) è una espressione e in quanto tale torna un valore
+%come in scheme e tutti i linguaggi funz.
+test_caseRitornaValore(ToTest) -> 
+	ValueRet = case ToTest of 
+		"caso1" -> 1;
+		"caso2" -> 2;
+		_ -> 3
+	end,
+	ValueRet + 1000.
