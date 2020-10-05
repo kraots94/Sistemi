810e25ed2d9a7524a36eac2132c40a1c530af470
Alessandro <aleforgia3@gmail.com>
2020-10-05 17:08:31 +0200

    Divisione moduli, da finire

--- src/records.hrl
+++ src/records.hrl
@@ -28,3 +28,6 @@
 -record(taxiListenerState, {pidMoving,
 							pidBattery,
 							pidElection}).
+
+% dati di inizializzazione del modulo gps
+-record(dataInitGPSModule, {pid_entity, type, pid_server_gps, starting_pos, signal_power}).
